a8d1b908bcd698b1b3eeea6381e7ec92023e339f
micozhang <micozhang@localhost>
2025-01-29 15:47:53 -0600

    Finish

--- src/studio2/Ruin.java
+++ src/studio2/Ruin.java
@@ -4,36 +4,52 @@ public class Ruin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int startAmount = 10;
+
 		double winChance = .5;
 		int winLimit = 20;
-		int currentAmount = startAmount;
-		int totalSimulations = 0;
-		
-		
-		while ((currentAmount > 0) && (currentAmount < 20))
+		int win=0;
+		int lose=0;
+		int simulations = 500;
+		int startAmount = 5;
+
+		for(int totalSimulations = simulations; totalSimulations>0; totalSimulations--)
 		{
-			double luck = Math.random();
-			if (luck >= .5)
+			
+			int currentAmount = startAmount;
+			while ((currentAmount > 0) && (currentAmount < 20))
 			{
-				currentAmount++;
-				System.out.println("Win");
+				double luck = Math.random();
+				if (luck >= .5)
+				{
+					currentAmount++;
+					//System.out.println("Win");
+				}
+				else
+				{
+					currentAmount--;
+					//System.out.println("Loss");
+				}
+			}
+			if (currentAmount == 0)
+			{
+				lose++;
+				System.out.println("Ruin");
 			}
 			else
 			{
-				currentAmount--;
-				System.out.println("Loss");
+				win++;
+				System.out.println("Success");	
 			}
-		}
-		if (currentAmount == 0)
-		{
-		System.out.println("Ruin");
-		}
-		else
-		{
-		System.out.println("Success");	
-		}
 
+		}
+		//int netwinloss = (int) (win-lose);
+		//System.out.println(netwinloss);
+
+		double ruinRate = (((double)lose)/simulations)*100;
+		System.out.println(ruinRate + "%");
+		
+		double expectedRuinRate = (1-(((double) startAmount)/winLimit))*100;
+		System.out.println("Expected Ruin Rate: " + expectedRuinRate + "%");
 	}
 
 }
